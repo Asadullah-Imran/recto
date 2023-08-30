772d21dae53f0d7d6c6936be20d449927b1c3d25
Asadullah-Imran <asadullahimran19@gmail.com>
2023-08-31 02:35:20 +0600

    30 aug 23

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,74 @@
+import React from "react";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./App.scss";
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
+import Add from "./pages/add/Add";
+import Gig from "./pages/gig/Gig";
+import Gigs from "./pages/gigs/Gigs";
+import Home from "./pages/home/Home";
+import Message from "./pages/message/Message";
+import Messages from "./pages/messages/Messages";
+import MyGigs from "./pages/myGigs/MyGigs";
+import Orders from "./pages/orders/Orders";
+
 function App() {
+  const Layout = () => {
+    return (
+      <div className="app">
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </div>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/gig/:id",
+          element: <Gig />,
+        },
+        {
+          path: "/gigs",
+          element: <Gigs />,
+        },
+        {
+          path: "/orders",
+          element: <Orders />,
+        },
+        {
+          path: "/mygigs",
+          element: <MyGigs />,
+        },
+        {
+          path: "/add",
+          element: <Add />,
+        },
+        {
+          path: "/messages",
+          element: <Messages />,
+        },
+        {
+          path: "/message",
+          element: <Message />,
+        },
+      ],
+    },
+  ]);
 
   return (
     <div>
-      Hello world!
+      <RouterProvider router={router} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.scss";
+const Navbar = () => {
+  const [active, setActive] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const isActive = () => {
+    window.scrollY > 0 ? setActive(true) : setActive(false);
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", isActive);
+
+    return () => {
+      window.removeEventListener("scroll", isActive);
+    };
+  }, []);
+
+  const currentUser = {
+    id: 1,
+    username: "John Doe",
+    isSeller: true,
+  };
+
+  return (
+    <div className={active ? "navbar active" : "navbar"}>
+      <div className="container">
+        <div className="logo">
+          <Link to="/" className="link">
+            <span className="text">Fiverr</span>
+          </Link>
+
+          <span className="dot">.</span>
+        </div>
+        <div className="links">
+          <span>Fiverr Business</span>
+          <span>Explore</span>
+          <span>English</span>
+          <span>Sign in</span>
+          {!currentUser?.isSeller && <span>Become a seller</span>}
+          {!currentUser && <button>Join</button>}
+          {currentUser && (
+            <div className="user" onClick={() => setOpen(!open)}>
+              <img src="./pp.png" alt="dp" />
+              <span>{currentUser?.username}</span>
+              {open && (
+                <div className="options">
+                  {currentUser?.isSeller && (
+                    <>
+                      <Link to="/gigs" className="link">
+                        Gigs
+                      </Link>
+                      <Link to="/add" className="link">
+                        Add New Gig
+                      </Link>
+                    </>
+                  )}
+                  <Link to="/orders" className="link">
+                    Orders
+                  </Link>
+                  <Link to="/messages" className="link">
+                    Messages
+                  </Link>
+                  <Link to="/" className="link">
+                    Log out
+                  </Link>
+                </div>
+              )}
+            </div>
+          )}
+        </div>
+      </div>
+
+      {active && (
+        <>
+          <hr />
+          <div className="menu">
+            <span>test</span>
+            <span>test2</span>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
